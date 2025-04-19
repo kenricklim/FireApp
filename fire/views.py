114a89f59c6072cfb2c177c34cc77d7c9fef62a4
Bry <etoquillab@gmail.com>
2025-04-20 01:42:21 +0800

    add a dashboard

--- fire/views.py
+++ fire/views.py
@@ -3,20 +3,19 @@ from django.views.generic.list import ListView
 from fire.models import Locations, Incident, FireStation
 
 
+
 class HomePageView(ListView):
     model = Locations
     context_object_name = 'home'
     template_name = "home.html"
 
+
 class ChartView(ListView):
-    template_name = 'chart.html'
+    template_name = 'Dchart/chart.html'
 
     def get_context_data(self, **kwargs):
         context = super(). get_context_data(**kwargs)
-        return context
-    
-    def get_queryset(self, *args, **kwargs):
-        pass
+        context["qs"] = Dchart
 
 
 def map_station(request):
--- projectsite/settings.py
+++ projectsite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "fire",
     "widget_tweaks",
+    "rest_framework"
 ]
 
 MIDDLEWARE = [
